689ed2c6293ed35d61b8ea350e448c22f856594a
Lukasz Siwiec <lukasz.siwiec@euvic.pl>
2020-04-24 13:26:42 +0200

    big letters

--- _test_/cesare13 .test.js
+++ _test_/cesare13 .test.js
@@ -2,7 +2,23 @@ var cesare13 = require('../cesare13');
 
 describe('cesare13', () => {
   it('should return a string', () => {
-    const results = cesare13();
+    const results = cesare13('');
     expect(typeof results).toBe('string');
+  });
+
+  it('should return a string', () => {
+    const results = cesare13('');
+    expect(typeof results).toBe('string');
+  });
+
+  it('should only accept string as parameter', () => {
+    expect(() => {
+      cesare13(1231);
+    }).toThrow();
+  });
+
+  it('should return NOP from ABC', () => {
+    const results = cesare13('ABC');
+    expect(results).toBe('NOP');
   });
 });
--- cesare13.js
+++ cesare13.js
@@ -1,5 +1,18 @@
-function cesare13() {
-  return '';
+function encodChar(char) {
+  const code = char.charCodeAt(0);
+  if (code >= 65 && code <= 90) return String.fromCharCode(((code - 65 + 13) % 26) + 65);
+}
+
+function cesare13(input) {
+  if (typeof input != 'string') {
+    throw Error('Accept only string as parameter!');
+  }
+  let results = '';
+  for (let char of input) {
+    const encoded = encodChar(char);
+    results += encoded;
+  }
+  return results;
 }
 
 module.exports = cesare13;
